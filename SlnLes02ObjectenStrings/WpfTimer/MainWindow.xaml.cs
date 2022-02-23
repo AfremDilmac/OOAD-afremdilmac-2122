420458b284d06c9af0bd9fb88ff8e446343f68b3
Afrem <71547964+AfremDilmac@users.noreply.github.com>
2022-02-23 18:56:16 +0100

    Verbetering

--- SlnLes02ObjectenStrings/WpfTimer/MainWindow.xaml.cs
+++ SlnLes02ObjectenStrings/WpfTimer/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfTimer
 {
@@ -20,9 +21,53 @@ namespace WpfTimer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer = new DispatcherTimer();
+        private int count;
         public MainWindow()
         {
-            InitializeComponent();
+            count = 300;
+            timer.Interval = TimeSpan.FromMilliseconds(1000);
+            timer.Tick += timer_Tick;
+
+
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //Hier is de count-- omdat ik de versie heb gekoze waar de timer van 5min aftelt en de achtergrondkleur verandert.
+            count--;
+            lblCount.Content = TimeSpan.FromSeconds(count).ToString(@"mm\:ss");
+
+            //Hiermee bepaal ik de hoogte van mijn rectangles via de aantal van de count.
+            rctSeconds.Height = count;
+            rctMinuts.Height = count / 5;
+
+
+            //Hier gebeurt de background color via de timer het start met rood aan 5min en wanneer ik aan 0 kom wordt het groen.
+            byte r = Convert.ToByte(17 + count / 2);
+            byte g = Convert.ToByte(255 - (17 + count / 2));
+            byte b = 0;
+            test.Background = new SolidColorBrush(Color.FromRgb(r, g, b));
+
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            //start de timer
+            timer.Start();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            //stopt de timer
+            timer.Stop();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Reset button count terug aan 0 en timer herstart.
+            timer.Stop();
+            count = 300;
+            lblCount.Content = count;
         }
     }
 }

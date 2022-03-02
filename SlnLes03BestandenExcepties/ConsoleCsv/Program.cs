63e80716ae716e1265585656b631ee68a24fc9ea
Afrem <71547964+AfremDilmac@users.noreply.github.com>
2022-03-02 13:07:19 +0100

    commit

---
+++ SlnLes03BestandenExcepties/ConsoleCsv/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCsv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // prepare
+            List<string> lines = new List<string>();
+            lines.Add("Dit is lijn 1");
+            lines.Add("Dit is lijn 2");
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string filePath = System.IO.Path.Combine(folderPath, "myfile.txt");
+            // open stream and start writing
+            using (StreamWriter writer = File.CreateText(filePath))
+            {
+                foreach (string line in lines)
+                {
+                    writer.WriteLine(line);
+                }
+            } // stream closes automatically
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SlnLes03BestandenExcepties/WpfCopyVs1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCopyVs1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            dialog.Filter = "Tekstbestanden|*.TXT;*.TEXT";
+            string chosenFileName;
+            bool? dialogResult = dialog.ShowDialog();
+            if (dialogResult == true)
+            {
+                // user picked a file and pressed OK
+                chosenFileName = dialog.FileName; // user accepted
+                FileInfo fi = new FileInfo(chosenFileName);
+                boxIn.Text += $"{fi.Name}";
+                boxOut.Text += $"out{fi.Extension}";
+                lblUitvoer.Content = $"bestand is overgezet";
+            }
+            else
+            {
+                lblUitvoer.Content = "geen bestand gekozen";
+            }
+        }
+    }
+}
